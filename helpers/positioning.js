8c1790a81dc8d99f816202e7cac02ba632880bfb
dami <dami@debian.debian.com>
2025-03-14 17:34:17 -0400

    removed place on Top

--- helpers/positioning.js
+++ helpers/positioning.js
@@ -44,34 +44,3 @@ export function placeModelsAtPoints(modelCreator, referenceSelector, pointsWithO
   });
 }
 
-/**
- * Creates and positions two models, with the top model placed on the bottom model
- * @param {Object} bottomObject - The bottom model instance
- * @param {Object} topObject - The top model instance
- * @returns {Array} Array containing [bottomObject, positionedTopObject]
- */
-export function placeOnTop(bottomObject, topObject) {
-  // Get bottom object's bounding box info
-  const bottomBounds = bottomObject.boundingBox.bounds;
-  const bottomCenter = centerSelector(bottomObject);
-  
-  // Calculate top center of bottom object's bounding box
-  const topCenterOfBottom = [bottomCenter[0], bottomCenter[1], bottomBounds[1][2]];
-  
-  // Get top object's bounds
-  const topBounds = topObject.boundingBox.bounds;
-  
-  // Calculate bottom center of top object's bounding box
-  const topCenter = centerSelector(topObject);
-  const bottomCenterOfTop = [topCenter[0], topCenter[1], topBounds[0][2]];
-  
-  // Calculate translation vector to align bottom center of top with top center of bottom
-  const translationVector = [
-    topCenterOfBottom[0] - bottomCenterOfTop[0],
-    topCenterOfBottom[1] - bottomCenterOfTop[1],
-    topCenterOfBottom[2] - bottomCenterOfTop[2]
-  ];
-  
-  // Return both objects, with top object translated
-  return [bottomObject, topObject.translate(translationVector)];
-}
